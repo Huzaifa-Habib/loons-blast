d89245f92a775b6421468472b804110a362c8706
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-08-02 17:30:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+var colors = ["#fca5a5","#c2410c","#a3e635","#86efac","#0891b2","#6d28d9","#1d4ed8","#fae8ff","#db2777",
+"#9d174d","#075985","#3f6212","#854d0e","#fcd34d","#f43f5e","#881337","	#00FF00"];
+
+
+
+
+function box1 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box1 = document.getElementById("box1");
+    box1.style.backgroundColor = random
+}
+
+function box2 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box2 = document.getElementById("box2");
+    box2.style.backgroundColor = random
+}
+
+
+function box3 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box3 = document.getElementById("box3");
+    box3.style.backgroundColor = random
+}
+
+
+function box4 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box4 = document.getElementById("box4");
+    box4.style.backgroundColor = random
+}
+
+
+function box5 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box5 = document.getElementById("box5");
+    box5.style.backgroundColor = random
+}
+
+
+function box6 () {
+    var random=colors[Math.floor(Math.random()*colors.length)]
+    var box6 = document.getElementById("box6");
+    box6.style.backgroundColor = random
+}
+
+
+
+
+
